7b81d9b452c2067fa54bb9b8ca91d0e3417a62bd
@eduardo_78d <eduardo78d@gmail.com>
2017-12-29 10:35:52 -0600

    models

--- test/models/fact_test.rb
+++ test/models/fact_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: facts
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :string
+#  real        :boolean          default(TRUE)
+#  provable    :boolean          default(FALSE)
+#  color       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  code        :string
+#
+
 require 'test_helper'
 
 class FactTest < ActiveSupport::TestCase
