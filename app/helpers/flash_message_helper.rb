27100279eb43866027c46aeed69e634fa0358b38
@eduardo_78d <eduardo78d@gmail.com>
2018-01-02 19:56:21 -0600

    changes

--- app/helpers/flash_message_helper.rb
+++ app/helpers/flash_message_helper.rb
@@ -7,7 +7,7 @@ module FlashMessageHelper
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, role:"alert", class: "alert #{bootstrap_class_for(msg_type)}" ) do 
-        #concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+        #-concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
         concat message 
       end)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
+
   devise_for :users
   get 'home/index'
 
   root to: "home#index"
 
   resources :facts
-  
+  resources :users, only: [:show]
+
 end
