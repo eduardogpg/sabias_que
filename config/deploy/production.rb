2fdc08319b5eeebaccb953b27a427a9ea79254bb
@eduardo_78d <eduardo78d@gmail.com>
2018-01-27 22:24:31 -0600

    change production rv

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,4 @@
-# Replace 127.0.0.1 with your server's IP address!
-#server '178.62.11.106', user: 'deploy', roles: :app
-#server '178.62.11.106', roles: %w(app web db), primary: true, user: 'deploy'
+set :stage, :production
 
-role :app, '178.62.11.106'
+# Replace 127.0.0.1 with your server's IP address!
+server '178.62.11.106', user: 'deploy', roles: %w{web app}
