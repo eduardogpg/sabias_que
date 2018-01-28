5a9e2291dc10e2368938deeb30da4780b54dbe74
@eduardo_78d <eduardo78d@gmail.com>
2018-01-27 21:48:29 -0600

    judas priest

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,3 @@
 # Replace 127.0.0.1 with your server's IP address!
-server '178.62.11.106', user: 'deploy', roles: :app
+#server '178.62.11.106', user: 'deploy', roles: :app
+role :app, '178.62.11.106'
